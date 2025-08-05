0fa4cfb9723e6ee589fc74ec0e3eb66f5bcdcc95
Vijay6615 <vishukla19@gmail.com>
2025-08-05 11:03:15 +0000

    datatypes

---
+++ 01_basics/01_problem.js
@@ -0,0 +1,16 @@
+// for declaration we use 2 one is const second is var 
+// const cannot be change 
+// var has two types one is let second one is var (var) is old so use more let
+//  { }this is a scope
+
+const accountId = 01111
+let accountEmail = "viju@gmail"
+var accountPassword = "12345"
+accountCity = "mumbai"
+let accountState;
+
+// accountId = 02   we can't change the id 
+accountCity = "lucknow"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict"   //  this treats all JS codes as a new version
+
+//alert( 3 + 3 )  we are using NodeJs not browser
+
+// console.log( 3 + 3) ; console.log("vijay")
+// do not write in this way
+// this makes code unreadable for us aur others
+
+let name = "vijay"    // this three i am using because [var] is old
+let age = 19
+let loggedIn = false
+
+// number => 2 to power 53
+// BigInt
+// string => " "
+// boolean => true / false
+// null => standalone Value
+// undefined =>
+// symbol => unique 
+
+
+// Object
+
+console.log(typeof "vijay") // string
+console.log(typeof age) // number
+console.log(typeof undefined) // undefined
+console.log(typeof null ) // object (interview question)
