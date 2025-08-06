6f035bcfd5aa4e60cc729957467ee2f28301bd76
Vijay6615 <vishukla19@gmail.com>
2025-08-06 06:58:14 +0000

    memory

--- 01_basics/02_datatype-summary.js
+++ 01_basics/02_datatype-summary.js
@@ -3,9 +3,9 @@
 
     // 7 types : string, nimber, boolean, null, undefined, symbols, Bigint
 
-    const id = Symbol('123')
-    const studentId = Symbol('123')
-    console.log(id === studentId);
+   //  const id = Symbol('123')
+   //  const studentId = Symbol('123')
+   //  console.log(id === studentId);
 
 
 
@@ -13,12 +13,43 @@
 
      // Array, Objects, Functions
 
-     const heros = ["vijay", "Ajay", "Abhay"];
-     let myObj = {
-        name: "Gaurav",
-        age: 22,
-     }
+   //   const heros = ["vijay", "Ajay", "Abhay"];
+   //   let myObj = {
+   //      name: "Gaurav",
+   //      age: 22,
+   //   }
 
-     const myFunction = Function(){
-        console.log("Hello World");
-     }
+   //   const myFunction = Function(){
+   //      console.log("Hello World");
+   //   }
+
+
+     //+++++++++++++++++  MEMORY +++++++++++++++++++++++++++++++++
+
+     // 2 type of memory ( stack , Heap)
+
+   // stack (Primitive) => copy
+   // Heap (non-Primitive) => original reference
+
+   //stack example
+   let myYoutubename = "vijay gaming"
+   let anotherName = myYoutubename
+   anotherName = "vijay blogs"
+   console.log(anotherName)
+      console.log(myYoutubename)
+   
+   //Heap example
+   let userOne = {
+      name: "vijay",
+      email: "user@google.com",
+      upi: "user@ybl"
+   };
+   let userTwo = userOne
+   userTwo.email = "xyzgoogle@.com"
+
+   console.log(userTwo.email)
+   console.log(userOne.name)
+
+    //  name:"gaurav"
+     //email:"gaurav@gmail.com"
+   
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # js-hindi
 A code repo for javascript series
 don't write messy code, write clean code!
+avoid using semicolon(:) in commented at last
