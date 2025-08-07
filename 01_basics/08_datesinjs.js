d4a96d44a9ffbc4da4a333ae5dc23f85abc7483e
Vijay6615 <vishukla19@gmail.com>
2025-08-07 13:31:45 +0000

    datesinjs

---
+++ 01_basics/08_datesinjs.js
@@ -0,0 +1,18 @@
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toTimeString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+let mycreatedDate = new Date(2025, 2,21)
+let myCreatedDate = new Date(2025, 1, 21, 8, 50)
+
+console.log(mycreatedDate);
+console.log(mycreatedDate.toDateString());
+console.log(myCreatedDate.toLocaleString());
+console.log(myCreatedDate.getDay());
+console.log(myCreatedDate.getDate());
+
