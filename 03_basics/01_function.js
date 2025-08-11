c9807f3fe5316b0f48b211e885037d3ff750ebc9
Vijay6615 <vishukla19@gmail.com>
2025-08-11 11:12:20 +0000

    more function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -1,4 +1,27 @@
-function addTwoNumbers (number1, number2){
-    console.log(number1 + number2)
+// function addTwoNumbers (number1, number2, number3){
+//     console.log(number1 + number2 + number3)
+// }
+
+function addTwoNumbers (number1, number2, number3){
+
+    // let result = number1 + number2 + number3     // method 1
+    // return result
+    return number1 + number2 + number3              // method 2 (easy)
 }
-addTwoNumbers(10,10)
+const result = addTwoNumbers(10,10,10)
+//console.log('result:', result);
+
+function addCartValue(...num1){
+    return num1
+}
+// console.log(addCartValue(100, 200, 300, 400, 500))
+
+const user = {
+    usernme: 'vijay',
+    price: 199
+}
+function getUser(anyUser){
+    console.log(`username is ${anyUser.usernme} and the price is ${anyUser.price}`)
+}
+console.log(user)       // { usernme: 'vijay', price: 199 }
+getUser(user)           // username is vijay and the price is 199
