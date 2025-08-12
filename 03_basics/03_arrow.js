049a75609570e9109eeaf8ec848089be5864a6ed
Vijay6615 <vishukla19@gmail.com>
2025-08-12 11:41:18 +0000

    arrow function

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -12,4 +12,19 @@ const user = {
 // user.username = 'ajay'
 // user.welcomeMessage()
 
-console.log(this);          // this gives empty obj 
+//console.log(this);          // this gives empty obj 
+
+
+// const addTwo = (number1 , number2) => {
+//     return number1 + number2                        // basic arrow function or in curly brackey we need to write return function
+// }
+
+// const addTwo = (number1 , number2) => number1 + number2     // implesit arrow function     
+
+const addTwo = (number1 , number2) => (number1 + number2 )         // more and more use in react or no need to write return function
+
+const addATwo = (number1 , number2) =>( {username: "Vijay"}  )       // for objects required paranthesis
+
+console.log(addTwo(2,8))
+console.log(addATwo())          // [Function: addATwo]
+console.log(addATwo)            // { username: 'Vijay' }
