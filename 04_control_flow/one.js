31bacb31c76af3223fd7f97e627a97a85a5e0167
Vijay6615 <vishukla19@gmail.com>
2025-08-13 06:11:04 +0000

    if and switch

---
+++ 04_control_flow/one.js
@@ -0,0 +1,41 @@
+
+// if (2==="2") {
+//     console.log('executed');  
+// }
+
+const score = 200
+// if (score >=100) {
+//     let power = "fly"
+//     console.log(`user power: ${power}`); 
+// }
+// //console.log(`user power: ${power}`);
+
+// if (score>100) console.log('yes');
+// if (score>100) console.log('yes'), console.log('haan');   NOT TO USE THIS TYPE
+
+
+// if (score>1000) {
+    // console.log('less' );
+// }else if (score<800) {
+    // console.log('less then 800');
+// } else{
+    // console.log('less');
+    // 
+// }
+
+
+// const isLoggedIn = true
+// const dabitCard = true
+// if (isLoggedIn && dabitCard) {
+//     console.log('eligible for cources');
+// }else{
+//     console.log('recheck required documents');
+    
+// }
+
+const loggedInGooogle = false
+const loggedInGithub = true
+if (loggedInGooogle || loggedInGithub) {
+    console.log('user logged in');
+    
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,14 @@
+const value = 50
+switch (value) {
+    case 20:
+        console.log('20 rupee');
+        break;
+    case 50:
+        console.log('50 rupee');
+        break;
+
+    default:
+        console.log('invalid');
+        
+        break;
+}
